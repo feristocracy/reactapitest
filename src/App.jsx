e3561f7c9d103339ec9d46e21112f0ef4585b4bb
Fernando Maldonado <fermaldonadosolis@gmail.com>
2022-10-17 18:40:55 -0500

    Starting react project with API

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/App/Header/Header'
+import { useState } from 'react'
+
+const App = () => {
+
+  const [contador, setContador] = useState(0)
+
+  return (
+    <>
+     <h1>Buenas</h1>
+    </>
+  )
+}
+
+export default App
